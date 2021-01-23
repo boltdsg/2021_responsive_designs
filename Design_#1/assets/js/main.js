5dd3cdd8739af2380e4c89bcf2f9f38849b62c41
samdsg <boltcode17@gmail.com>
2021-01-23 00:28:27 -0800

    with marsi

--- Design_#1/assets/js/main.js
+++ Design_#1/assets/js/main.js
@@ -1,6 +1,8 @@
 /*===== SHOW MENU =====*/
 /*===== ACTIVE AND REMOVE MENU =====*/
-/*===== CHANGE BACKGROUND HEADER =====*/
+/*===== CHANGE BACKGROUND HEADER =====*/ 
+/*===== SWIPER CAROUSEL =====*/
 /*===== SWIPER CAROUSEL =====*/
 /*===== SHOW SCROLL TOP =====*/
+/*===== GSAP ANIMATION =====*/
 /*===== GSAP ANIMATION =====*/
